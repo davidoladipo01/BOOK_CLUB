c2dc4a137a24cf9da540a2f08995efe89a4842c8
David Oladipo <davidoladipo2025@gmail.com>
2026-07-06 12:37:49 +0100

    feat: add Pg1 component and implement hero section in Home page

---
+++ src/components/Pg1.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from './Button'
+import { Link } from 'react-router-dom'
+
+const Pg1 = () => {
+  return (
+    <div>
+      <section>
+        <div className="home-container">
+        <div className="hero-overlay"></div>
+          <div className="hero-content">
+            <span className='hero-content1'>AfriReadCo</span>
+            <div className='hero-content2'>Africa's Home for Readers</div>
+            <div className='hero-content3'>Because every great African story deserves a reader who truly connects with it.</div>
+            <Button to="/register" className='mt-6 inline-block rounded-xl shadow-md bg-black button focus:outline-none focus:ring-2' />
+            <a className='underline mt-6  py-3 px-5 text-sm' href=""><Link to={"/login"}>Sign In</Link></a>
+          </div>
+
+          <div>
+
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Pg1
